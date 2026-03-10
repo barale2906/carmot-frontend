0dedc83cdcc4d665bb1740ffd010f1894ae187b2
barale2906 <alexanderbarajas@gmail.com>
2026-03-10 00:06:58 -0500

    Ajuste buil

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import { notify } from '@/composables/useNotification'
 
 // Configuración base de axios
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
